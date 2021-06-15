3200ace1c7e8396931a1ceba9f0e6d00b9f75077
sushil11th <sushil11th@gmail.com>
2021-06-15 21:29:46 +0530

    Register and Deregister lambda fucntion

---
+++ boto3/Lambda/ALB-Reg-Dreg/lambda_function-de.py
@@ -0,0 +1,63 @@
+import boto3
+import json
+import time
+from collections import defaultdict
+
+region = 'us-west-1'
+
+def lambda_handler(event, context):
+    HTTPS_PROXY = 'http://primary-proxy.gslb.intranet.barcapint.com:8080'
+    elbv2client = boto3.client('elbv2')
+    
+    client = boto3.client('ec2')
+
+    running_instances = client.describe_instances(
+      Filters=[
+        {
+            'Name': 'tag:INFRA:Hostname',
+            'Values': [
+                'duwdsr002070613.intranet.barcapint.com',
+            ]
+        },
+    ],
+    )
+    
+    instance_ids = []    
+    
+    for reservation in running_instances['Reservations']:
+        for instance in reservation['Instances']:
+            instance_ids.append(instance['InstanceId'])
+        
+    #return instance_ids
+    for each in instance_ids:
+        #print(each)
+    #return each
+    
+        healthtarget = elbv2client.describe_target_health(
+        TargetGroupArn='arn:aws:elasticloadbalancing:eu-west-1:632210341693:targetgroup/vault-albtg-vlt-csm-dev/d5018c0bc84243d4',
+        Targets=[
+        {
+            'Id': each,
+        }
+        ]
+        )
+        print("healthtarget = ", healthtarget)
+        
+        deregister_response = elbv2client.deregister_targets(
+        TargetGroupArn='arn:aws:elasticloadbalancing:eu-west-1:632210341693:targetgroup/vault-albtg-vlt-csm-dev/d5018c0bc84243d4',
+        Targets=[
+        {
+            'Id':each,
+        }
+        ]
+        )
+        print("deregister_response = ",  deregister_response)
+        time.sleep(310)
+        response=client.stop_instances(
+            'Id':each,
+            )
+    return {
+        
+        'statusCode': 200,
+        'body': json.dumps(deregister_response)
+    }
